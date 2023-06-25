03875a6009d5e96da7fb325a3bd7d3402000cb66
MrRoolade <128980419+MrRoolade@users.noreply.github.com>
2023-06-25 11:04:08 +0200

    comments added

--- Eau02.py
+++ Eau02.py
@@ -1,13 +1,17 @@
-## 
-## affiche la transformation d'une heure affichée en format 24h en une heure affichée en format 12h
+##<~~MrRøølåÐe~~>
+## Combinaisons de 2 chiffres
+## affiche toutes les combinaisons possible de 2 nombres entre 00 et 99 dans l'ordre croissant.
 
 ## Variables Import et Fonction
-n = 100
+n = 100 # définit l'interval pour l'affichage des chiffres
 
+# ci dessous 2 boucles imbriquées
+# la premiere itère x jusqu'à 99
+# la deuxième itère y de x jusqu'à 99; si y = x on saute le chiffre sinon on affiche la combinaison
 for x in range(n) :
     for y in range(x,n) :
         if y == x :
             pass
         else :
-            print (f"{x:02d} {y:02d},",end=" ")
+            print (f"{x:02d} {y:02d},",end=" ") #affiche la combinaison des chiffres avec 2 digits
 #FIN
