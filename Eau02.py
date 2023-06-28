47d1879ef757b07ea3ba39c91afd504e9294726d
MrRoolade <128980419+MrRoolade@users.noreply.github.com>
2023-06-28 07:20:18 +0200

    re organising code with guide line

--- Eau02.py
+++ Eau02.py
@@ -1,22 +1,26 @@
 #~> MrRøølåÐe <~#
 ## ARGUMENTS à L'ENVERS
-## 
+## programme qui affiche ses arguments reçus à l’envers
+
 import sys
 
-# verif_digit = False                                           #si les arguments doivent etre que des chiffres
-Nbre_Arg = len(sys.argv)
-
-if Nbre_Arg > 1:
-
-    # verif_digit = any(arg.isdigit() for arg in sys.argv)      #si les arguments doivent etre que des chiffres
-
-    # if not verif_digit:                                       #si les arguments doivent etre que des chiffres
-
-        for i in range(Nbre_Arg-1,0,-1):
+# Fonction
+def invert_arg() :
+     for i in range(Nbre_Arg-1,0,-1):
             if sys.argv != sys.argv[0]: 
-                print(sys.argv[i])
+                new_array.append(sys.argv[i])
+    
+# Parsing
+Nbre_Arg = len(sys.argv)
+new_array =[]
 
-    # else :                                                    #si les arguments doivent etre que des chiffres
-        # print("error")
-else :
+# Gestion des erreurs
+if Nbre_Arg <= 1:
     print("error")
+    
+# Résolution
+invert_arg()
+
+# Résultat
+for i in range(Nbre_Arg-1):
+    print(new_array[i])
