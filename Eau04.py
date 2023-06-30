337c457d8d00ae29551d83f0bd0648c84c731fdb
MrRoolade <128980419+MrRoolade@users.noreply.github.com>
2023-06-30 15:28:50 +0200

    final release prochain nombre premier

--- Eau04.py
+++ Eau04.py
@@ -1,5 +1,5 @@
 #~~> MrRøølåÐe <~~#
-## la suite de Fibonacci
+## Prochain nombre premier
 ### programme qui affiche le premier nombre premier supérieur au nombre donné en argument.
 #### Affiche -1 si le paramètre est négatif ou mauvais.
 
@@ -7,7 +7,7 @@ import sys
 
 # Fonctions
 def is_prime(n):
-    for x in range(2, n-1) :            # boucle sur l'interval de nombre entre 2 et la saisie -1
+    for x in range(2, n-1) :
         if n % x == 0 :  
             return(False)
     return (True)
@@ -18,7 +18,7 @@ def next_prime_number(user_value):
         n += 1
     return(n)
 
-def sortie_prog():
+def quit_program():
     print("-1")
     sys.exit()
 
@@ -29,16 +29,13 @@ number_of_value = len(sys.argv)
 # Gestion des erreurs   
 if number_of_value == 2 :
     try:
-        user_value.isdigit()
         user_value = int(user_value)
-
         if user_value <= 0 :
-            sortie_prog()
-
+            quit_program()
     except (ValueError, IndexError): #si erreur de type de variable
-        sortie_prog()
+        quit_program()
 else:
-    sortie_prog()
+    quit_program()
 
 # Résolution
 result = next_prime_number(user_value)
