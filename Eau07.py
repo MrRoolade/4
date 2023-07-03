c7dbb717795a309b80e08d03ac8850486b14297b
MrRoolade <128980419+MrRoolade@users.noreply.github.com>
2023-07-03 21:05:32 +0200

    Majuscule

--- Eau07.py
+++ Eau07.py
@@ -1,5 +1,5 @@
 #~~> MrRøølåÐe <~~#
-## Majuscule sur 2
+## Majuscule
 ### programme qui met en majuscule la première lettre de chaque mot d’une chaîne de caractères. Les autres lettres devront être en minuscules.
 #### Les mots ne sont délimités que par un espace, une tabulation ou un retour à la ligne.
 ##### Afficher error et quitter le programme en cas de problèmes d’arguments.
